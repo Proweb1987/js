4460ff47508385a27f00837e031e5eee4b7f2e61
vitalik <87@.ru>
2021-12-16 21:28:29 +0500

    end

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 
-var myName = 'Vitalik';
+
 var str = prompt ('Введите свое имя');
-console.log( str + 'Меня зовут Виталик');
+console.log('Меня зовут Виталик' + str);
 
 var str = prompt('сколько вам лет');
-console.log(str + 'Мне 20 лет');
+console.log('Мне 20 лет' + str);
 
 var str = prompt ('Решите пример ' + '6 + 3 = ?')
-console.log(str + 'Пример1: ' + '6 + 3 = 9');
+console.log('Пример1: ' + '6 + 3 = 9') + str;
 
 var str = prompt ('Решите пример ' + '6 - 3 = ?')
-console.log(str + 'Пример2: ' + '6 - 3 = 3');
+console.log('Пример2: ' + '6 - 3 = 3' + str);
 
 var str = prompt ('Решите пример ' + '6 * 3 = ?')
-console.log(str + 'Пример3: ' + '6 * 3 = 18');
+console.log('Пример3: ' + '6 * 3 = 18' + str);
 
 var str = prompt ('Решите пример ' + '6 / 3 = ?')
-console.log(str + 'Пример4: ' + '6 / 3 = 2');
+console.log('Пример4: ' + '6 / 3 = 2' + str);
 
 var str = prompt ('Решите пример ' + '6 % 3 = ?')
-console.log(str + 'Пример5: ' + '6 % 3 = 0');
-
+console.log('Пример5: ' + '6 % 3 = 0' + str);
+var Name = 'Открыть консоль для просмотра';
+alert(Name)
